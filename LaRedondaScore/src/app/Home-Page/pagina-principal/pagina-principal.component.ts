d7480a9943f23d91dfe78ab0e2375c608c6abad7
Etienne2021 <etiennediablo11@gmail.com>
2024-11-08 18:40:04 -0300

    Pagina principal

--- LaRedondaScore/src/app/Home-Page/pagina-principal/pagina-principal.component.ts
+++ LaRedondaScore/src/app/Home-Page/pagina-principal/pagina-principal.component.ts
@@ -7,11 +7,12 @@ import { BarraLateralComponent } from '../barra-lateral/barra-lateral.component'
 import { NavComponent } from '../../navbar/nav/nav.component';
 import { BuscarComponent } from '../../search/buscar/buscar.component';
 import { LoginPageComponent } from "../../Login/login-page/login-page.component";
+import { EquiposFavoritosComponent } from '../equipos-favoritos/equipos-favoritos.component';
 
 @Component({
   selector: 'app-pagina-principal',
   standalone: true,
-  imports: [RouterModule, CommonModule, PartidosComponent, BarraLateralComponent, NavComponent, BuscarComponent, LoginPageComponent],
+  imports: [RouterModule, CommonModule, PartidosComponent, BarraLateralComponent, NavComponent, BuscarComponent, LoginPageComponent,EquiposFavoritosComponent],
   templateUrl: './pagina-principal.component.html',
   styleUrl: './pagina-principal.component.css'
 })
