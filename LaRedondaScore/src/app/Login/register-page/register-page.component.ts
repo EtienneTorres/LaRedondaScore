8ead63ae17beed68be99554a0a8856a9fec57f16
Marianoribas02 <mariano.ribas5191@gmail.com>
2024-11-13 21:47:11 -0300

    Correccion cartel registro.

--- LaRedondaScore/src/app/Login/register-page/register-page.component.ts
+++ LaRedondaScore/src/app/Login/register-page/register-page.component.ts
@@ -7,42 +7,42 @@ import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-register-page',
   standalone: true,
-  imports: [FormsModule,CommonModule],
+  imports: [FormsModule, CommonModule],
   templateUrl: './register-page.component.html',
   styleUrl: './register-page.component.css'
 })
 export class RegisterPageComponent {
 
   username: string = ''; // Nombre de usuario
-  password: string = ''; // Contrasena
-  message: string = '';
+  password: string = ''; // Contraseña
+  message: string = ''; // Mensaje de error
+  successMessage: string = ''; // Mensaje de éxito
 
   // Constructor 
-  constructor(private user : UserService, private router: Router) {}
+  constructor(private user: UserService, private router: Router) {}
 
-  // Metodo para registrar a un nuevo usuario
+  // Método para registrar a un nuevo usuario
   onRegister(): void {
     this.user.register(this.username, this.password).subscribe(
       response => {
         console.log('Registro exitoso', response);
-        alert('Usuario registrado con éxito');
+        this.successMessage = 'Usuario registrado con éxito';
 
+        // Oculta el mensaje después de 4 segundos y redirige a login
         setTimeout(() => {
-          // Cierra la ventana de registro
+          this.successMessage = '';
           window.close();
-        }, 4000);
-  
-        // Redirige a la página de login
-        this.router.navigate(['/login-page']);
-  
-        // Cierra la ventana o pestaña actual
-        window.close();
+        }, 1000);
       },
       error => {
         console.error('Error en el registro', error);
         this.message = 'Error en el registro. Intente de nuevo.';
+
+        // Oculta el mensaje de error después de unos segundos
+        setTimeout(() => {
+          this.message = '';
+        }, 4000);
       }
     );
   }
-  
 }
